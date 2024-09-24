a1f2cd35e2a8439617817f1af0199cc90b10d61d
CromvOrr <strek.oliwier@gmail.com>
2024-09-25 00:45:03 +0200

    Added interactive data visualization

--- figures.py
+++ figures.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import pandas as pd
+import plotly.express as px
 
 
 def generate_plots(_df, years_list):
@@ -19,3 +22,177 @@ def generate_plots(_df, years_list):
         plt.tight_layout()
         plt.show()
         _fig.savefig(f'plot_{year}.png')
+
+
+def plot_01(_df, __df, years_list):
+    fig = go.Figure()
+    for year in years_list:
+        _df_year = _df[['name', year]].rename(columns={year: 'value'})
+        _df_year['source'] = 'ŚREDNIA'
+
+        __df_year = __df[['name', year]].rename(columns={year: 'value'})
+        __df_year['source'] = 'MEDIANA'
+
+        year_data = pd.concat([_df_year, __df_year])
+        fig.add_trace(go.Bar(
+            x=year_data[year_data['source'] == 'ŚREDNIA']['name'],
+            y=year_data[year_data['source'] == 'ŚREDNIA']['value'],
+            name=f'{year} ŚREDNIA',
+            visible=(year == '2010'),
+            marker_color='blueviolet'
+        ))
+        fig.add_trace(go.Bar(
+            x=year_data[year_data['source'] == 'MEDIANA']['name'],
+            y=year_data[year_data['source'] == 'MEDIANA']['value'],
+            name=f'{year} MEDIANA',
+            visible=(year == '2010'),
+            marker_color='crimson'
+        ))
+
+    steps = []
+    for i, year in enumerate(years_list):
+        step = dict(
+            method='update',
+            args=[{'visible': [False] * len(fig.data)}],
+            label=year
+        )
+        for j in range(i * 2, i * 2 + 2):
+            step['args'][0]['visible'][j] = True
+        steps.append(step)
+
+    sliders = [dict(
+        active=0,
+        steps=steps,
+        x=0.0,
+        y=-0.5,
+        currentvalue={'prefix': 'Rok: ', 'font': {'size': 20}},
+        pad={'t': 50},
+    )]
+    fig.update_layout(
+        sliders=sliders,
+        title='Średnia i mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych',
+        xaxis_title='Województwo',
+        yaxis_title='Cena za 1 m2 [PLN]',
+        barmode='group',
+        height=600,
+        xaxis=dict(
+            tickangle=-90,
+            tickmode='array',
+            tickvals=list(range(len(_df['name']))),
+            ticktext=_df['name']
+        )
+    )
+    fig.show()
+
+
+def plot_02(_df):
+    _df = _df.melt(id_vars=['name'], var_name='year', value_name='value')
+    names = _df['name'].unique()
+    global_min = _df['value'].min()
+    global_max = _df['value'].max()
+
+    fig = go.Figure()
+    for name in names:
+        name_data = _df[_df['name'] == name]
+        fig.add_trace(go.Bar(
+            x=name_data['year'],
+            y=name_data['value'],
+            name=name,
+            visible=(name == 'MAŁOPOLSKIE'),
+            marker_color='blueviolet'
+        ))
+
+    steps = []
+    for i, name in enumerate(names):
+        step = dict(
+            method='update',
+            label=name,
+            args=[{'visible': [False] * len(fig.data)}, {'yaxis': {'range': [global_min, global_max]}}]
+        )
+        step['args'][0]['visible'][i] = True
+        steps.append(step)
+
+    sliders = [dict(
+        active=0,
+        steps=steps,
+        x=0.0,
+        y=-0.1
+    )]
+    fig.update_layout(
+        sliders=sliders,
+        title='Liczba mieszkań oddanych do użytkowania w latach 2010 - 2022',
+        xaxis_title='',
+        yaxis_title='',
+        yaxis=dict(range=[global_min, global_max]),
+        height=600
+    )
+    fig.show()
+
+
+def plot_03(_df, __df):
+    _df_melted = _df.melt(id_vars=['name'], var_name='Year', value_name='Value')
+    __df_melted = __df.melt(id_vars=['name'], var_name='Year', value_name='Value')
+
+    fig = px.scatter(_df_melted, x='Year', y='Value', color='name',
+                     title='Przeciętne miesięczne wynagrodzenia brutto')
+
+    fig.add_trace(go.Scatter(
+        x=__df_melted['Year'],
+        y=__df_melted['Value'],
+        mode='markers',
+        name='POLSKA',
+        marker=dict(color='black', size=10, symbol='x')
+    ))
+    fig.update_yaxes(range=[2000, 8500])
+    fig.update_layout(
+        xaxis_title='Rok',
+        yaxis_title='Kwota [PLN]',
+        legend_title=''
+    )
+    fig.show()
+
+
+def plot_04(_df):
+    fig = go.Figure()
+    for year in range(2010, 2023):
+        _df_sorted = _df.sort_values(str(year))
+
+        fig.add_trace(
+            go.Bar(
+                y=_df_sorted['name'],
+                x=_df_sorted[str(year)],
+                orientation='h',
+                visible=False,
+                name=str(year)
+            )
+        )
+
+    fig.data[0].visible = True
+    steps = []
+    for i, year in enumerate(range(2010, 2023)):
+        step = dict(
+            method='update',
+            args=[{'visible': [False] * len(fig.data)}],
+            label=str(year)
+        )
+        step['args'][0]['visible'][i] = True
+        steps.append(step)
+
+    sliders = [dict(
+        active=0,
+        currentvalue={'prefix': 'Rok: '},
+        pad={'t': 50},
+        steps=steps
+    )]
+    fig.update_traces(marker_color='blueviolet')
+    fig.update_layout(
+        sliders=sliders,
+        xaxis_title='Zyskowność sprzedaży brutto [%]',
+        yaxis_title='Województwo',
+        title='Wyniki finansowe przedsiębiorstw (PKD 2007) - wskaźniki',
+        barmode='stack',
+        autosize=False,
+        height=600,
+        width=1700
+    )
+    fig.show()
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import pandas as pd
 import requests
 import os
 import plot_viewer as pv
+import figures as fig
 
-var_id = op.get_variable_id('P2412')  # FINANSE PUBLICZNE / DOCHODY BUDŻETÓW WOJEWÓDZTW / DOCHODY NA 1 MIESZKAŃCA
+# FINANSE PUBLICZNE / DOCHODY BUDŻETÓW WOJEWÓDZTW / DOCHODY NA 1 MIESZKAŃCA
+var_id = op.get_variable_id('P2412')
 op.print_list(var_id)
 
 years = [str(year) for year in range(2010, 2023)]
@@ -30,3 +32,39 @@ dbf_data['name'] = dbf_data['name'].apply(op.normalize_text)
 
 merged_df = pd.merge(dbf_data, var_data_df, on='name')
 pv.run(merged_df, years)
+
+# RYNEK NIERUCHOMOŚCI / RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH / ŚREDNIA CENA ZA 1 M2 LOKALI MIESZKALNYCH SPRZEDANYCH
+# W RAMACH TRANSAKCJI RYNKOWYCH
+var_id = op.get_variable_id('P3788')
+var_data = op.get_variable_data('633692', years)
+var_data_df = pd.DataFrame(var_data)
+# RYNEK NIERUCHOMOŚCI / RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH / MEDIANA CEN ZA 1 M2 LOKALI MIESZKALNYCH SPRZEDANYCH
+# W RAMACH TRANSAKCJI RYNKOWYCH
+var_id_cmp = op.get_variable_id('P3787')
+var_data_cmp = op.get_variable_data('633677', years)
+var_data_cmp_df = pd.DataFrame(var_data_cmp)
+
+fig.plot_01(var_data_df, var_data_cmp_df, years)
+
+# PRZEMYSŁ I BUDOWNICTWO / BUDOWNICTWO MIESZKANIOWE / MIESZKANIA ODDANE DO UŻYTKOWANIA
+var_id = op.get_variable_id('P3824')
+var_data = op.get_variable_data('748601', years)
+var_data_df = pd.DataFrame(var_data)
+
+fig.plot_02(var_data_df)
+
+# WYNAGRODZENIA I ŚWIADCZENIA SPOŁECZNE / WYNAGRODZENIA / PRZECIĘTNE MIESIĘCZNE WYNAGRODZENIA BRUTTO
+var_id = op.get_variable_id('P2497')
+var_data = op.get_variable_data('64428', years)
+var_data_df = pd.DataFrame(var_data)
+var_data_cmp = op.get_variable_data('64428', years, '0')
+var_data_cmp_df = pd.DataFrame(var_data_cmp)
+
+fig.plot_03(var_data_df, var_data_cmp_df)
+
+# FINANSE PRZEDSIĘBIORSTW (DANE KWARTALNE) / WYNIKI FINANSOWE PRZEDSIĘBIORSTW / WYNIKI FINANSOWE (PKD 2007) - WSKAŹNIKI
+var_id = op.get_variable_id('P3869')
+var_data = op.get_variable_data('1548847', years)
+var_data_df = pd.DataFrame(var_data)
+
+fig.plot_04(var_data_df)
